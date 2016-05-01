08300b7713bea0fd354da32042c376fc048fc91b
Vincent Behar <v.behar@free.fr>
2016-05-01 21:09:08 +0200

    cleanup factory code

--- pkg/openshift/flags.go
+++
@@ -1,9 +0,0 @@
-package openshift
-
-import (
-	"github.com/spf13/pflag"
-)
-
-var (
-	Flags = pflag.NewFlagSet("openshift-git", pflag.ContinueOnError)
-)
--- pkg/openshift/init.go
+++ pkg/openshift/init.go
@@ -3,4 +3,14 @@ package openshift
 import (
 	// install all required openshift resources
 	_ "github.com/openshift/origin/pkg/api/install"
+
+	"github.com/openshift/origin/pkg/cmd/util/clientcmd"
+
+	"github.com/spf13/pflag"
+)
+
+// init the factory early, to bind the flags
+var (
+	Flags   = pflag.NewFlagSet("openshift-git", pflag.ContinueOnError)
+	Factory = clientcmd.New(Flags)
 )
